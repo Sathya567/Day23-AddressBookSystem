e6561ef82ef398c3b54f32b0d1103935497b9b01
BootCampBL <satyabhandari43@gmail.com>
2021-08-02 22:33:18 +0530

    [satya] added- Main method of Address Book

---
+++ Day23-AddressBookSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23_AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book System");
+
+
+            while (true)
+            {
+                Console.WriteLine("=======================================================================");
+                Console.WriteLine("1. Add member to Contact list \n2.View Members in Contact List\n3.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        UC1_AddressBookMain.AddMember();
+                        break;
+                    case 2:
+                        UC1_AddressBookMain.ViewContacts();
+                        break;
+
+                    case 3:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+            }
+        }
+    }
+}
+
